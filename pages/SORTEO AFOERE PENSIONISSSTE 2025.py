39b6dc53c1666c7ceb08d303f4c547d18f5ff8d6
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-01-07 17:05:14 -0600

    Update SORTEO AFOERE PENSIONISSSTE 2025.py

--- pages/SORTEO AFOERE PENSIONISSSTE 2025.py
+++ pages/SORTEO AFOERE PENSIONISSSTE 2025.py
@@ -9,6 +9,22 @@ import streamlit as st
 import json
 import os
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Archivo donde se guardarán los comentarios
 comments_file = 'sorteo.json'
 
