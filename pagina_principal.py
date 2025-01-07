05db795fa922a45fa612191f1a84f22eea1348b9
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-01-07 17:04:51 -0600

    Update pagina_principal.py

--- pagina_principal.py
+++ pagina_principal.py
@@ -7,6 +7,22 @@ Created on Tue Jan  7 16:52:26 2025
 
 import streamlit as st
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Título y descripción de la aplicación
 st.set_page_config(page_title="Foro Comunitario de AFORE PENSIONISSSTE", page_icon="💬", layout="wide")
 st.title("💬 Foro Comunitario de AFORE PENSIONISSSTE")
